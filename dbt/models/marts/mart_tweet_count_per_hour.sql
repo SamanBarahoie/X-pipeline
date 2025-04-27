6204a3bf3170fd512514a91de5583995ff520555
wizard <88585655+samanbraa1@users.noreply.github.com>
2025-04-27 20:50:06 +0330

    Analytics DB ready1

---
+++ dbt/models/marts/mart_tweet_count_per_hour.sql
@@ -0,0 +1,17 @@
+
+{{ config(materialized='table') }}
+
+WITH tweet_counts AS (
+    SELECT
+        DATE_TRUNC('hour', created_at) AS tweet_hour,
+        COUNT(*) AS tweet_count
+    FROM {{ ref('stg_twitter_data') }}
+    WHERE cleaned_text IS NOT NULL
+    GROUP BY 1
+)
+
+SELECT
+    tweet_hour,
+    tweet_count
+FROM tweet_counts
+ORDER BY tweet_hour;
